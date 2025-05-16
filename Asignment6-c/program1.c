c0c457c038a0dc06a345e903a37a8dc67c6cca13
apurva8377 <apurvashinde098@gmail.com>
2025-05-16 20:48:49 +0530

    Add files via upload

---
+++ Asignment6-c/program1.c
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     program1.c
+//  Descreption :   Program which accepts number from user and if number is less than 50 then print small, 
+//                  if it is greater than 50 and less than 100 then print medium, if it is greater than 100 
+//                  then print large.
+//  Author :        Apurva Vilas Shinde
+//  Date :          16/05/2025
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+void Number(int iNo)
+{
+   if(iNo <50)
+   {
+    printf("Number is Small.\n");
+   } 
+   if((iNo >= 50) && (iNo <= 100))
+   {
+    printf("Number is Medium.\n");
+   }
+   if(iNo > 100)
+   {
+    printf("Number is large.\n");
+   }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number :\n");
+    scanf("%d",&iValue);
+
+    Number(iValue);
+
+    return 0;
+}
---
+++ Asignment6-c/program5.c
@@ -0,0 +1,39 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//  File name :     program5.c
+//  Descreption :   program which accept number from user and display its table in reverse order.
+//  Author :        Apurva Vilas Shinde
+//  Date :          16/05/2025
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Table(int iNo)
+{
+    int iCnt = 0;
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 10; iCnt >= 1; iCnt--)
+    {
+        iNo * iCnt;
+        printf("%d\t",iNo * iCnt);
+    }
+    printf("\n");
+    
+}
+
+int main()
+{
+    int iValue = 0;
+    
+    printf("Enter number :\n");
+    scanf("%d",&iValue);
+
+    Table(iValue);
+
+    return 0;
+}
