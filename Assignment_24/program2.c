28eaa58e1b3db211d000d761f2d020fe782490c0
apurva8377 <apurvashinde098@gmail.com>
2025-06-06 15:03:06 +0530

    Add files via upload

---
+++ Assignment_24/program2.c
@@ -0,0 +1,43 @@
+/////////////////////////////////////////////////////////
+//
+//  File name :     program2.c
+//  Description :   Write a program which accepts a string from user and counts the number of small characters.
+//  Author :        Apurva Vilas Shinde.
+//  Date :          04/06/2025
+//
+//////////////////////////////////////////////////////////
+
+// Input: Marvellous
+// Output: 9
+
+#include <stdio.h>
+
+int CountSmall(char *str)
+{
+    int iCnt = 0;
+
+    while (*str != '\0')
+    {
+        if (*str >= 'a' && *str <= 'z')
+        {
+            iCnt++;
+        }
+        str++;
+    }
+    return iCnt;
+}
+
+int main()
+{
+    char Arr[50];
+    int iRet = 0;
+
+    printf("Enter String: ");
+    scanf("%[^\n]", Arr);
+
+    iRet = CountSmall(Arr);
+
+    printf("Number of Small letters = %d\n", iRet);
+
+    return 0;
+}
